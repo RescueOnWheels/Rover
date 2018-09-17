a69fa48ff29e0f00ce330ad68ca3f302f82b5285
Martijn Vegter <mail@mvegter.me>
2018-09-17 13:30:22 +0200

    Fixed the fuck out of #601!
    Updated to latest Larynx and integrated `disconnect` event.

--- rover.js
+++ rover.js
@@ -2,12 +2,13 @@ const Motor = require('./Motor');
 const Larynx = require('./Larynx');
 
 const comm = new Larynx();
+const motor = new Motor();
 
 comm.on('move', (data) => {
     console.log(data);
+
     if(data == 'left') {
         motor.Left();
-        return;
     } else if (data == 'right') {
         motor.Right();
     } else if (data.speed == 0 || data.speed == -0) {
@@ -15,6 +16,10 @@ comm.on('move', (data) => {
     } else {
         motor.Forward(data);
     }
+});
+
+comm.on('disconnect', () => {
+    motor.Stop();
 });
 
 comm.Connect();
