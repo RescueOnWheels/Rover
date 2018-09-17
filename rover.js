9c4e7c849e0f98d79d27ffae2384128a3170bbb6
Christiaan <christiaanvanarum2@gmail.com>
2018-09-17 12:41:33 +0200

    less french words
    Added iffies

--- rover.js
+++ rover.js
@@ -5,7 +5,16 @@ const comm = new Larynx();
 
 comm.on('move', (data) => {
     console.log(data);
-
-})
+    if(data == 'left') {
+        motor.Left();
+        return;
+    } else if (data == 'right') {
+        motor.Right();
+    } else if (data.speed == 0 || data.speed == -0) {
+        motor.Stop();
+    } else {
+        motor.Forward(data);
+    }
+});
 
 comm.Connect();
