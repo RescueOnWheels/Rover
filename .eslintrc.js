8044a6309efefc248d08dc77506d4ab04bad2bbb
Martijn Vegter <martijn@martijnvegter.com>
2018-09-25 11:07:41 +0200

    Version control is awful
    Updated ESLint and added Travis support.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,45 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        "multiline-comment-style": ["error", "starred-block"],
+        "no-multiple-empty-lines": ["error", {
+          "max": 1,
+          "maxBOF": 0,
+          "maxEOF": 1
+        }],
+        "require-jsdoc": ["error", {
+          "require": {
+            "FunctionDeclaration": true,
+            "MethodDefinition": true,
+            "ClassDeclaration": true,
+            "ArrowFunctionExpression": true,
+            "FunctionExpression": true
+          }
+        }],
+        "spaced-comment": ["error", "always", {
+          "block": {
+            "balanced": true
+          }
+        }],
+        "valid-jsdoc": ["error", {
+          "prefer": {
+            "arg": "param",
+            "argument": "param",
+            "class": "class",
+            "return": "returns",
+            "virtual": "abstract"
+          },
+          "preferType": {
+            "boolean": "Boolean",
+            "number": "Number",
+            "object": "Object",
+            "string": "String"
+          },
+          "matchDescription": ".+",
+          "requireParamDescription": true,
+          "requireReturn": true,
+          "requireReturnDescription": true,
+          "requireReturnType": true,
+        }]
+    }
 };
