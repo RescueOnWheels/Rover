54171296715f39aaaf32daf6cf7bf4415568cff4
Martijn Vegter <mail@mvegter.me>
2018-10-17 22:08:39 +0200

    More tests

--- test/mock/specifications.test.js
+++ test/mock/specifications.test.js
@@ -2,20 +2,61 @@
 const chai = require('chai');
 
 /* Test target */
+const Larynx = require('./../../lib/Larynx');
 const Motor = require('./../../lib/Motor');
 
 const should = chai.should();
 
 module.exports = () => {
+  describe('Larynx', () => {
+    it('should extend from EventEmitter.', () => {
+      // Arrange
+      const superName = Object.getPrototypeOf(Object.getPrototypeOf(Larynx)).constructor.name;
+
+      // Assert
+      superName.should.equal('EventEmitter');
+    });
+
+    it('should expose the `on` function.', () => {
+      // Assert
+      should.exist(Larynx.on);
+
+      // Assert
+      Larynx.on.should.be.an('function');
+    });
+  });
+
   describe('Motor', () => {
     it('should expose the `Left` function.', () => {
+      // Assert
       should.exist(Motor.Left);
+
+      // Assert
       Motor.Left.should.be.an('function');
     });
 
+    it('should expose the `Movement` function.', () => {
+      // Assert
+      should.exist(Motor.Movement);
+
+      // Assert
+      Motor.Movement.should.be.an('function');
+    });
+
     it('should expose the `Right` function.', () => {
+      // Assert
       should.exist(Motor.Right);
+
+      // Assert
       Motor.Right.should.be.an('function');
+    });
+
+    it('should expose the `Stop` function.', () => {
+      // Assert
+      should.exist(Motor.Stop);
+
+      // Assert
+      Motor.Stop.should.be.an('function');
     });
   });
 };
