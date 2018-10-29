8f14f4ce9088d5c313ae210c7dc9b2b0f79b3dca
Martijn Vegter <mail@mvegter.me>
2018-10-29 19:15:21 +0100

    Need more tests

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,5 +1,13 @@
+const Motor = require('./../../lib/Motor/test/test');
+const Nervi = require('./../../lib/Nervi/test/test');
+
 const specifications = require('./specifications.test');
 
 describe('Mock', () => {
   describe('specifications', specifications);
+
+  describe('lib', () => {
+    describe('Motor', Motor);
+    describe('Nervi', Nervi);
+  });
 });
