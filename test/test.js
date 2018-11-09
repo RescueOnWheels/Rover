a83f21cec7af1e0d3c8890afcb671248bf0ce869
Martijn Vegter <martijn@martijnvegter.com>
2018-11-09 11:15:56 +0100

    I'm hungry. (#62)

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 let toMockOrNotToMockThatIsTheQuestion;
+
+/* istanbul ignore else */
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'CI') {
   // eslint-disable-next-line no-console
   console.warn('Rover: Using the mock tests!');
